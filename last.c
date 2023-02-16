bcd6e3165cddfa3f40d892e35ac6e604296087f7
Jerry <jerrythottatil@gmail.com>
2023-02-16 16:27:34 +0530

    created last.c file

--- last.c
+++ last.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("hjtrh");
-   return 0;
+int main(void)
+{
+int a,b,choice,result;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+printf("1 is addition \n2 is subtraction \n3 is multiplication \n4 is division \n:");
+scanf("%d",&choice);
+
+if(choice==1){
+    result=a+b;
+}
+else if(choice==2) {
+    result=a-b;
+}
+else if(choice==3){
+    result=a*b;
+}
+else if(choice==4){
+    result=a/b;
+}
+else {
+    printf("Fool");
+}
+printf("result = %d",result);
+
+    return 0;
 }
