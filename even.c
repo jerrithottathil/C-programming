601d904b4160de0aa18e8adcb97e49a259b91224
jerrithottathil <65187975+jerrithottathil@users.noreply.github.com>
2023-02-21 16:09:23 +0530

    Update even.c

--- even.c
+++ even.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+for finding even numbers we have to take an if condition with modulos, modulus % means its a reminder 
+after division for example if we divie 4%2 the answer will be 2 with reminder zero so we can say that 
+alll even numbers will be divisible by 2 and gives reminder zero
 int main(){
     int i,n;
     printf("enter the number:");
